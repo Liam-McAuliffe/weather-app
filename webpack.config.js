a5c03c7a9a7dd444522d7f6d3a602e2d755a0488
Liam-McAuliffe <lmmcauliffe1223@gmail.com>
2025-04-06 11:16:05 -0400

    Production

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
